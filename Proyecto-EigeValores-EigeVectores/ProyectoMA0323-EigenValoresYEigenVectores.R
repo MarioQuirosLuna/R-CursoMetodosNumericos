2a9c801b4c75685462c70fadfbbc509e5c12be9c
Mario Quiros Luna <marioql.dev@gmail.com>
2022-06-29 13:47:00 -0600

    Proyecto Valores y vectores propios

--- Proyecto-EigeValores-EigeVectores/ProyectoMA0323-EigenValoresYEigenVectores.R
+++ Proyecto-EigeValores-EigeVectores/ProyectoMA0323-EigenValoresYEigenVectores.R
@@ -1,9 +1,25 @@
-n <- 3
-m <- 3
+## Ejemplo 1 (2 , -1) : Resultado => Valores Propios L=3, L=1 
+##           (-1 , 2)             => Vectores Propios V1(-1,1), V2(1,1)
+
+## Ejemplo 2 (0 , 0.5): Resultado => Valores Propios L=0.5, L=-0.5 
+##           (0.5 , 0)            => Vectores Propios V1(1,1), V2(1,-1)
+
+## Ejemplo 3 (3 , 4)  : Resultado => Valores Propios L=4, L=-1 
+##           (1 , 0)              => Vectores Propios V1(4,1), V2(1,-1)
+
+n <- 2
+m <- 2
 
 matriz <- matrix(data=0, n,  m)
 
 data.entry(matriz) 
 
 eigen(matriz, symmetric = isSymmetric(matriz))
+
+
+0.2425356 * 4
+
+
+4 * 0.1767767
+-4 * 0.1767767
 
